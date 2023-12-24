5ce82d55ef5224640bd317173371af42e980d1a3
Umar Pervaiz <umarpervaiz425@gmail.com>
2023-12-24 14:53:17 +0500

    Built Login Page and setup redirections

--- configsql.php
+++ configsql.php
@@ -1,17 +1,47 @@
 <?php
-session_start();
-$servername = "";
-$username = "";
-$password = "";
-
+$serverName = "OmersMachine\SQLEXPRESS";
+$databaseName = "final";
 try {
-  $mysqlserver = new PDO("sqlsrv:server=$servername;Database=FSWeb", $username, $password);
-  // set the PDO error mode to exception
- // $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
- $mysqlserver->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//  echo "Success: ";
-} catch(PDOException $e) {
-  echo "Connection failed: ";
+  // Create a PDO instance
+  $pdo = new PDO("sqlsrv:Server=$serverName;Database=$databaseName", null, null);
+  // Set PDO error mode to exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully<br>";
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Retrieve form data
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  echo "$username<br>";
+  echo "$password<br>";
+
+  // SQL query to insert data into the Society table
+  $sql = "SELECT * FROM Students WHERE FirstName = ? AND Password = ?";
+  $params = array($username, $password);
+  echo "Got here";
+  // Prepare and execute the query
+  $stmt = $pdo->prepare($sql);
+  echo "Got here";
+  $stmt->execute($params);
+
+  // Fetch additional data
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if ($row != "") {
+    // Login successful, redirect to the success page
+    header("Location: success.php");
+    exit(); // Ensure that no further code is executed after the redirect
+  } else {
+    // Invalid credentials, redirect to the error page
+    // Invalid credentials, redirect to the error page with parameters
+    //header("Location: error.php?username=" . urlencode($username) . "&//password=" . urlencode($password) . "&data1=" . urlencode//($additionalData1) . "&data2=" . urlencode($additionalData2));
+
+    header("Location: error.php");
+    exit(); // Ensure that no further code is executed after the redirect
+  }
+}
 ?>
---
+++ error.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Error</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .container {
+            text-align: center;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 40px;
+            max-width: 400px;
+            width: 100%;
+        }
+
+        h1 {
+            color: #FF5722;
+            margin-bottom: 20px;
+        }
+
+        p {
+            color: #333;
+            margin-bottom: 30px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #008CBA;
+            font-weight: bold;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Login Error</h1>
+        <p>Invalid credentials. Please check your username and password and try again.</p>
+        <p>Need help? Contact support at <a href="mailto:support@example.com">support@example.com</a></p>
+        <p>
+            <a href="index.php">Go back to login page</a>
+        </p>
+    </div>
+</body>
+</html>
+
